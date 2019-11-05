c3c6a18b5ddb9ba23af08ebce6aaa668295fe04d
anthony <13207608300@163.com>
2019-11-05 20:51:30 +0800

    Prepare to test

--- tcpserver.js
+++ tcpserver.js
@@ -1,9 +1,13 @@
 const net = require('net');
 const TCP_PORT = "9000"
 const TIMEOUT = 60000;//tcp客户端超过60秒没发数据判为超时并断开连接
+const translate = require('./trans.js');
+const {createData} = require('./mongodb.js');
 // var tcpClient = null;//tcp客户端
 
-var receiveState;
+var receiveState = 0;
+var receivedData = [];
+
 const tcpServer = net.createServer((socket)=>{
     //connect
     let addr = socket.address().address + ':' + socket.address().port;
@@ -13,44 +17,59 @@ const tcpServer = net.createServer((socket)=>{
   
     // recieve data
     socket.on("data",data=>{
-      let str = addr+" receive: " + data.toString('Hex') + '\n';
+      let rd = data.toString('Hex');
+      let str = addr+" receive: " + rd + '\n';
       console.log(str);
       //console.log("Ascii data:",data.toString("Ascii"));
-      let headId = receivedData.substring(0,4);
-      if(headId==='5a5b')
-        socket.lastValue = Handle(receivedData);
+      let headId = rd.substring(0,4);
+      if(headId==='5a5b'){
+        if(receivedState<=5){
+          let analyseData = translate(rd);
+          receiveState ++;
+          receivedData.push(...analyseData);
+        }
+      }
       else{
         console.log("incorrect headId");
       }
     });
-  
+    
     // close
     socket.on('close',()=>{
       console.log(addr,"close");
-      tcpClient = null;
+      if(receiveState>0){
+        console.log(`Save ${receiveState} receivedData`);
+        createData(receivedData);
+        console.log("Save completely");
+        receiveState = 0;
+        receivedData = [];
+      }
     });
   
     socket.on('error',(err)=>{
+      receiveState = 0;
+      receivedData = [];
       console.log("error",err);
-      tcpClient = null;
     });
   
     socket.setTimeout(TIMEOUT);
       // 超过一定时间 没接收到数据，就主动断开连接。
+      receiveState = 0;
+      receivedData = [];
       socket.on('timeout', () => {
           console.log(socket.id,socket.addr,'socket timeout');
       socket.end();
-      tcpClient = null;
       });
-  });
+});
   
-  tcpServer.on("error",(err)=>{
+tcpServer.on("error",(err)=>{
+    receiveState = 0;
+    receivedData = [];
     console.log(err);
-    tcpClient = null;
-  });
+});
   
- tcpServer.listen({port: TCP_PORT,host: '0.0.0.0'}, () => {
+tcpServer.listen({port: TCP_PORT,host: '0.0.0.0'}, () => {
     console.log('tcp server running on', tcpServer.address())
-  });
+});
 
 module.exports = tcpServer;
